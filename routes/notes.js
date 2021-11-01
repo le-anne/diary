2afbeb0bf95c6f88c6230ddd9c7b72d316ecdd3f
Leanne <leannevictoriaharris@gmail.com>
2021-11-01 16:29:39 -0700

    initial commit

---
+++ routes/notes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import verifyToken from "../middleware/verifyToken";
+import notes from "../controllers/notes";
+
+const mongoose = require('mongoose');
+
+export default app => {
+  const router = express.Router();
+
+  router.use('/:username', [verifyToken]);
+
+  router.post("/:username/add", notes.createNote);
+  router.put("/:username/edit/:id", notes.updateNote);
+  router.delete("/:username/remove/:id", notes.deleteNote);
+  router.get("/:username/note/:id", notes.getNote);
+  router.get("/:username/notes", notes.getAllNotes);
+
+  app.use('/', router);
+};
+
