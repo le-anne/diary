55a250e4c3e77de4192afb7de735b21013096e08
Leanne <leannevictoriaharris@gmail.com>
2021-11-01 20:52:13 -0700

    updated connection to mongodb via connection.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import auth from "./routes/auth";
+import auth from "./routes/auth"
 import notes from "./routes/notes"
 
 mongoose.connect("mongodb://localhost/personaldiary_db", {
@@ -10,22 +10,8 @@ mongoose.connect("mongodb://localhost/personaldiary_db", {
   useUnifiedTopology: true
 });
 
-const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {}
-
-const myEmitter = new MyEmitter();
-// increase the limit
-myEmitter.setMaxListeners(11);
-
-for(let i = 0; i < 11; i++) {
-  myEmitter.on('event', _ => console.log(i));
-}
-
-myEmitter.emit('event');
-
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 app.use(cors());
 
@@ -33,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("It works!");
+  res.send("It works!" );
 });
 
 auth(app);
